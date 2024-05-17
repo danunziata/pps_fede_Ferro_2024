bb46f28af8766ed937c0aff53856a9b92d92161a
Federico Rodriguez <federf2018@live.com>
2024-05-17 14:34:36 -0300

    feat(iot): new setup file

---
+++ src/iot/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+minikube delete
+
+minikube start --driver=kvm2
+
+eval $(minikube docker-env)
+
+docker build -t mocksensor mocksensor
+
+kubectl apply -f pvc.yaml
+
+export MINIKUBEIP=$(minikube ip)
+
+envsubst < configmap.yaml | kubectl apply -f -
+
+kubectl apply -f service.yaml
+
+sleep(2)
+
+kubectl apply -f deployment.yaml
