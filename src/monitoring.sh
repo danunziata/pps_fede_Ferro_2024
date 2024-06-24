98aae670cd347c4d31ce1edae13d44958704ed02
Federico Rodriguez <federf2018@live.com>
2024-06-24 11:50:30 -0300

    feat(kubernetes): Re-edited installation scripts

--- src/monitoring.sh
+++ src/monitoring.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-echo -e "\n"
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
+# Define variables
+NAMESPACE="loki"
+VALUES_FILE="values/lokistack-values.yaml"
 
-kubectl create namespace loki
-helm upgrade --install loki grafana/loki-stack --namespace=loki --values values/lokistack-values.yaml
-echo -e "\n"
 echo -e "This might take a few minutes...\n"
-sleep 120
-kubectl get all -n loki
-echo -e "\nAdmin's Password"
-kubectl get secret loki-grafana --namespace=loki -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
-echo -e "\nTo access the grafana's service, you need to open a new terminal and execute the following command:"
-echo "kubectl port-forward --namespace loki service/loki-grafana 3000:80"
-echo -e "\nhttp::/localhost:3000"
+
+# Add Helm repo and update
+{
+    helm repo add grafana https://grafana.github.io/helm-charts
+    helm repo update
+
+    # Create Kubernetes namespace
+    kubectl create namespace $NAMESPACE
+
+    # Install/upgrade Loki stack
+    helm upgrade --install loki grafana/loki-stack --namespace=$NAMESPACE --values $VALUES_FILE
+
+    # Wait for the Loki Grafana deployment to be ready
+    kubectl rollout status deployment loki-grafana -n $NAMESPACE --timeout=210s
+} > /dev/null 2>&1
+
+# Check if the previous block executed successfully
+if [ $? -eq 0 ]; then
+    echo -e "The Loki stack has been deployed to your cluster. Loki can now be added as a datasource in Grafana."
+else
+    echo -e "An error occurred during the deployment of the Loki stack. Please check the logs for more details."
+    exit 1
+fi
+
+# Retrieve and display Grafana admin password
+echo -e "Admin's Password"
+kubectl get secret loki-grafana --namespace=$NAMESPACE -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+
+# Instructions to access Grafana service
+echo -e "\nTo access the Grafana service, open a new terminal and execute the following command:"
+echo "kubectl port-forward --namespace $NAMESPACE service/loki-grafana 3000:80"
+echo -e "\nThen, open your browser and go to:"
+echo "http://localhost:3000"
--- src/setups/laboratory.sh
+++ src/setups/laboratory.sh
@@ -1,21 +1,59 @@
 #!/bin/bash
 
-multipass find --force-update   
+default(){
+    multipass find --force-update > /dev/null 2>&1
+    multipass launch jammy --cpus 2 --memory 3G --disk 5G --name master-node --cloud-init ~/cluster/multipass.yaml
+    sleep 5
+    export MULTIPASSIP=`multipass info master-node | grep IP | grep -oE '[^ ]+$'`
+    sleep 2
+}
 
-clear
+default2(){
+    export KUBECONFIG=$PWD/kubeconfig
+    kubectl config use-context default
+    kubectl get node -o wide
+}
 
-multipass launch jammy --cpus 2 --memory 2G --disk 5G --name master-node --cloud-init ~/cluster/multipass.yaml
+echo -e "\nYou are installing k3s with multipass and k3sup..."
+echo -e "\nPlease select one of the following options"
+echo -e "\n[1] Default"
+echo -e "[2] Disable ServiceLB"
+echo -e "[3] Disable Traefik"
+echo -e "[4] Disable both"
+echo -e "\n"
 
-sleep 2
+valid_option=false
 
-export MULTIPASSIP=`multipass info master-node | grep IP | grep -oE '[^ ]+$'` 
+while [ "$valid_option" == false ]; do
+    read -p "Option: " option
 
-sleep 2
-
-k3sup install --ip $MULTIPASSIP --user ubuntu --k3s-extra-args "--cluster-init --disable=servicelb"
-
-export KUBECONFIG=$PWD/kubeconfig
-
-kubectl config use-context default
-
-kubectl get node -o wide
+    case $option in
+        1)
+            default
+            k3sup install --ip $MULTIPASSIP --user ubuntu --k3s-extra-args "--cluster-init" --local-path ~/config/kubeconfig
+            default2
+            valid_option=true
+            ;;
+        2)
+            default
+            k3sup install --ip $MULTIPASSIP --user ubuntu --k3s-extra-args "--cluster-init --disable=servicelb" --local-path ~/config/kubeconfig
+            default2
+            valid_option=true
+            ;;
+        3)
+            default
+            k3sup install --ip $MULTIPASSIP --user ubuntu --k3s-extra-args "--cluster-init --disable=traefik" --local-path ~/config/kubeconfig
+            default2
+            valid_option=true
+            ;;
+        4)
+            default
+            k3sup install --ip $MULTIPASSIP --user ubuntu --k3s-extra-args "--cluster-init --disable=traefik,servicelb" --local-path ~/config/kubeconfig
+            default2
+            valid_option=true
+            ;;
+        *)
+            echo -e "Invalid Option. Please try again."
+            ;;
+    esac
+done
